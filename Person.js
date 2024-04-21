8ac278881a499bdf8fa8d38c0a16e0002757d010
Girmachew Gulint <girmachewabebe@gmail.com>
2024-04-21 08:57:02 +0000

    Mongoose connection and Perosn Schema

--- Person.js
+++ Person.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose')
-const personSchema = new mongoose.Schema({
-    name:{type:String, required: true},
-    age:{type:Number},
-    favoriteFoods :{type:[String]}
-})
-
-const Person = mongoose.model('Person',personSchema)
+c
--- myApp.js
+++ myApp.js
@@ -6,7 +6,13 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 .then(()=>console.log("Connected"))
 .catch((err, res)=>console.log("Error"+err))
 let Person;
+const personSchema = new mongoose.Schema({
+    name:{type:String, required: true},
+    age:{type:Number},
+    favoriteFoods :{type:[String]}
+})
 
+Person = mongoose.model('Person',personSchema)
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
